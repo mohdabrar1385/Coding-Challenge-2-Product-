3b6b7be8af9cb0513d9ceda9047052bc6460d3d6
Mohammed Abrar Ahmed <mohdabrar1385@gmail.com>
2021-11-20 20:13:46 +0530

    Room App

---
+++ roomapp/src/main/java/com/smartherd/roomapp/repository/UserRepository.kt
@@ -0,0 +1,27 @@
+package com.smartherd.roomapp.repository
+
+import androidx.lifecycle.LiveData
+import com.smartherd.roomapp.data.UserDao
+import com.smartherd.roomapp.model.User
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User){
+        userDao.addUser(user)
+    }
+
+    suspend fun updateUser(user: User){
+        userDao.updateUser(user)
+    }
+
+    suspend fun deleteUser(user: User){
+        userDao.deleteUser(user)
+    }
+
+    suspend fun deleteAllUsers(){
+        userDao.deleteAllUsers()
+    }
+
+}
